0d938d1364a78c3acb7269c8e5f22c6e6ec6280f
anjgkay321 <hallohallosh@naver.com>
2025-04-23 17:27:56 +0900

    2025-04-23

---
+++ ex02_Parameter/src/main/java/com/example/app/controller/SimpleController.java
@@ -0,0 +1,40 @@
+package com.example.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+@RequestMapping("/test")
+public class SimpleController {
+	
+	@RequestMapping(value = "/test1",method=RequestMethod.GET)
+	public void test1() {
+		log.info("GET /test/Test1...");
+		// 파라미터
+		// 유효성
+		// 서비스
+		// 뷰
+	}
+
+	@RequestMapping(value = "/test2",method=RequestMethod.GET)
+	public String test2() {
+		log.info("GET /test/Test2...");
+		return "test/abcd";
+		// 파라미터
+		// 유효성
+		// 서비스
+		// 뷰
+	}
+	@RequestMapping(value = "/test3",method= {RequestMethod.GET,RequestMethod.POST})
+	public void test3() {
+		log.info("GET /test/Test3...");
+		// 파라미터
+		// 유효성
+		// 서비스
+		// 뷰
+	}
+}
---
+++ ex02_Parameter/src/main/webapp/WEB-INF/views/param/page01.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>PAGE01</h1>
+	USERNAME : ${dto.username}<br/>
+	AGE : ${dto.age}<br/>
+	ADDR : ${dto.addr}<br/>
+	
+	${test1} ${test2} ${test3} ${test4}$ {test5}
+</body>
+</html>
