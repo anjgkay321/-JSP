5e4a7f40ca7c0e7ab29a77588749c641b9930660
anjgkay321 <hallohallosh@naver.com>
2025-04-25 17:06:40 +0900

    2025-04-25

--- ex05_dataSource/src/test/java/DbTests/DataSourceTests.java
+++ ex05_dataSource/src/test/java/DbTests/DataSourceTests.java
@@ -24,6 +24,10 @@ class DataSourceTests {
 	private DataSource dataSource1;
 	
 	@Autowired
+	private DataSource dataSource2;
+	
+	
+	@Autowired
 	private MemoDaoImpl memoDaoImpl;
 	
 	@Test
@@ -35,10 +39,28 @@ class DataSourceTests {
 		pstmt.executeUpdate();
 	}
 	@Test
+	@Disabled
 	void test2() throws Exception {
 		memoDaoImpl.insert(new MemoDto(1,"a","a",LocalDateTime.now(),null));
 	}
 	
+	@Test
+	@Disabled
+	void test3() throws Exception {
+		System.out.println(dataSource2);
+		Connection con =  dataSource2.getConnection();
+		PreparedStatement pstmt = con.prepareStatement("insert into tbl_book values('ffff','ffff','ffff','ffff')");	
+		pstmt.executeUpdate();
+	}
+	@Autowired
+	private DataSource dataSource3;
+	@Test
+	void test4() throws Exception {
+		System.out.println(dataSource3);
+		Connection con =  dataSource3.getConnection();
+		PreparedStatement pstmt = con.prepareStatement("insert into tbl_book values('fqqwff','ffff','ffff','ffff')");	
+		pstmt.executeUpdate();
+	}
 }
 
 
