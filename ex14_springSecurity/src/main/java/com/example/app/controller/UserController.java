c904995a7f3e3c1bcea95d7d5c10fcd46003ed05
anjgkay321 <hallohallosh@naver.com>
2025-04-30 17:34:48 +0900

    2025-04-30

--- ex14_springSecurity/src/main/java/com/example/app/controller/UserController.java
+++ ex14_springSecurity/src/main/java/com/example/app/controller/UserController.java
@@ -6,6 +6,9 @@ import java.time.format.DateTimeFormatter;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,15 +17,20 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.app.domain.dto.UserDto;
+import com.example.app.domain.service.UserServiceImpl;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
 public class UserController {
-
+	
+	@Autowired
+	private UserServiceImpl userService;
+	
 	@InitBinder
 	public void dataBinder(WebDataBinder webDataBinder) {
 		log.info("UserController dataBinder ..." + webDataBinder);
@@ -30,18 +38,70 @@ public class UserController {
 		webDataBinder.registerCustomEditor(LocalDate.class, "birthday", new BirthdayEditor());
 	}
 	
+	
+	@GetMapping("/login")
+	public void login() {
+		log.info("GET /login...");
+	}
+	
+//	@GetMapping("/user")
+//	public void user(Authentication authentication) {
+//		log.info("GET /user..." + authentication);
+//		log.info("name..." + authentication.getName());
+//		log.info("principal..." + authentication.getPrincipal());
+//		log.info("authorities..." + authentication.getAuthorities());
+//		log.info("details..." + authentication.getDetails());
+//		log.info("credential..." + authentication.getCredentials());
+//	}
+	
+//	@GetMapping("/user")
+//	public void user(@AuthenticationPrincipal Principal principal) {
+//		log.info("GET /user..." + principal);
+//	}
+	
+	@GetMapping("/user")
+	public void user(Model model) {
+		log.info("GET /user...");
+		Authentication authenticaton = 
+		SecurityContextHolder.getContext().getAuthentication();
+		log.info("authentication : " + authenticaton);
+		
+		model.addAttribute("auth",authenticaton);
+		
+	}
+	
+	@GetMapping("/manager")
+	public void manager() {
+		log.info("GET /manager...");	
+	}
+	@GetMapping("/admin")
+	public void admin() {
+		log.info("GET /admin...");	
+	}
+	
+	
+	
 	@GetMapping("/join")
 	public void join() {
 		log.info("GET /join..");
 	}
 	@PostMapping("/join")
-	public void join_post(@Valid UserDto dto,BindingResult bindingResult,Model model) {
+	public String join_post(@Valid UserDto dto,BindingResult bindingResult,Model model,RedirectAttributes redirectAttributes ) {
 		log.info("POST /join.." + dto);
 		
 		for(FieldError error : bindingResult.getFieldErrors()) {
 			log.info("Error Field : "+error.getField()+" Error Msg : "+error.getDefaultMessage());
 			model.addAttribute(error.getField(),error.getDefaultMessage());
+			return "join";
 		}
+		
+		boolean isJoin  = userService.userJoin(dto);
+		if(isJoin) {
+			redirectAttributes.addFlashAttribute("message","회원가입 완료!");
+			return "redirect:/login";
+		}
+		else
+			return "join";
 	}
 	
 	private static class PhoneEditor extends PropertyEditorSupport {
--- ex14_springSecurity/src/main/webapp/WEB-INF/views/join.jsp
+++ ex14_springSecurity/src/main/webapp/WEB-INF/views/join.jsp
@@ -13,18 +13,22 @@
 			<h1>회원가입</h1>
 		</div>
 		<div>
-			<label>userid : </label><span style="color:red;font-size:.7rem;">${userid}</span><br>
-			<input name="userid" />
+			<label>username : </label><span style="color:red;font-size:.7rem;">${username}</span><br>
+			<input name="username" />
 		</div>
 		<div>
 			<label>password : </label><span style="color:red;font-size:.7rem;">${password}</span><br>
 			<input name="password" />
 		</div>
+
+
+<%--
 		<div>
 			<label>rePassword : </label><span style="color:red;font-size:.7rem;">${rePassword}</span><br>
 			<input name="rePassword" />
 		</div>
-		<div>
+		
+ 		<div>
 			<label>username : </label><span style="color:red;font-size:.7rem;">${username}</span><br>
 			<input name="username" />
 		</div>
@@ -48,6 +52,8 @@
 			<label>birthday : </label><span style="color:red;font-size:.7rem;">${birthday}</span><br>
 			<input type="text" name="birthday" placeHolder="ex)2025~01~01" />
 		</div>
+		 --%>
+		
 		<div>
 			<input type="submit" value="회원가입" />
 		</div>
